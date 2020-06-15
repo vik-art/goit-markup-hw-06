4df798298ec6fc4d61aadbda3814ca96dea8b77d
Viktoriia <krjuchenkova@gmail.com>
2020-06-15 17:21:24 +0300

    finish

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,16 @@
         refs.modal.classList.toggle('is-hidden');
     }
 })();
+(() => {
+    document
+        .querySelector('.modal-form')
+        .addEventListener('submit', e => {
+            e.preventDefault();
+
+            new FormData(e.currentTarget).forEach((value, name) =>
+                console.log(`${name}: ${value}`),
+            );
+
+            e.currentTarget.reset();
+        });
+})();
